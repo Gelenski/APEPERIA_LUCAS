19c3a8c6fc6750dde8f023e7e27667a228a1f162
Lucas Gelenski <gelenskilucas@gmail.com>
2024-03-13 15:41:59 -0300

    Criando o Modal

---
+++ script_modal.js
@@ -0,0 +1,14 @@
+const modal = document.getElementById("modal");
+const botao = document.getElementById("botaoModal");
+const span = document.getElementsByClassName("close")[0];
+botao.onclick = function () {
+  modal.style.display = "block";
+};
+span.onclick = function () {
+  modal.style.display = "none";
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
